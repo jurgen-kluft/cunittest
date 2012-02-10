7603a6e6d807648108a818b37e92adf9a0d97522
jinlin <jinlin@cnshaw492.virtuosgames.com>
2012-02-10 14:33:21 +0800

    * disable some old implement in TimeHelpers_ps3_spu to avoid spu crash

--- source/main/cpp/ut_TimeHelpers_ps3_spu.cpp
+++ source/main/cpp/ut_TimeHelpers_ps3_spu.cpp
@@ -10,20 +10,25 @@ namespace UnitTest
 {
     Timer::Timer()
     {
+#if 0
         uint64_t* frequency = reinterpret_cast<uint64_t*>(&mData);
         //*frequency = sys_time_get_timebase_frequency();
         assert(*frequency);
+#endif
     }
 
     void Timer::start()
     {
+#if 0
         system_time_t* startTime = reinterpret_cast<system_time_t*>(&mData[2]);
         //*startTime = sys_time_get_system_time();
         assert(*startTime);
+#endif
     }
 
     int Timer::getTimeInMs() const
     {
+#if 0
         //system_time_t curTime = sys_time_get_system_time();
 		system_time_t curTime = 0;
         assert(curTime);
@@ -38,6 +43,8 @@ namespace UnitTest
 		double const  seconds   = double(elapsedTime) / 1000.0;
 
         return int(seconds);
+#endif
+		return 0;
     }
 
     void TimeHelpers::sleepMs(int const ms)
