7098063c29293f7aa78f7c19a0c4c843f5dfc0bf
Li Jian" <lijian_a889@virtuos.com.cn>
2012-02-10 18:19:41 +0800

    * Add SPU exception handler on PPU side

--- source/spu_bootloader/cpp/main.cpp
+++ source/spu_bootloader/cpp/main.cpp
@@ -6,6 +6,7 @@
 #include <sys/spu_thread.h>
 #include <sys/spu_thread_group.h>
 #include <sys/spu_utility.h>
+#include <sys/ppu_thread.h>
 #include <sys/event.h>
 #include <sys/paths.h>
 #include <sys/process.h>
@@ -19,6 +20,8 @@ SYS_PROCESS_PARAM(1001, 0x10000)
 #define NUM_SPU_THREADS        1 /* The number of SPU threads in the group */ 
 #define PRIORITY             100
 //#define SPU_PROG       (SYS_APP_HOME "/event_receiver.spu.self")
+
+#define QUEUE_SIZE             32
 
 #include <spu_printf.h>
 
@@ -35,6 +38,13 @@ int main(int argc, char** argv)
 		{"SPU Thread 0"}; /* The names of SPU threads */
     sys_spu_image_t spu_img;
 
+	/*
+	 * Variables for the event queue and event handler PPU thread
+	 */
+	sys_event_queue_t queue;
+	sys_event_queue_attribute_t queue_attr;
+	sys_ppu_thread_t event_handle_thread; 
+
 	int ret;
 	
 	/*
@@ -49,6 +59,49 @@ int main(int argc, char** argv)
 	ret = sys_spu_initialize(MAX_PHYSICAL_SPU, MAX_RAW_SPU);
 	if (ret != CELL_OK) {
 		fprintf(stderr, "sys_spu_initialize failed: %#.8x\n", ret);
+		exit(ret);
+	}
+
+	/*
+	 * Initialize the event queue
+	 * This event queue will be connected to the SPU threads.
+	 */
+	printf("Creating an event queue.\n");
+	queue_attr.attr_protocol = SYS_SYNC_PRIORITY;
+	queue_attr.type = SYS_PPU_QUEUE;
+	ret = sys_event_queue_create(&queue, &queue_attr, SYS_EVENT_QUEUE_LOCAL, 
+								 QUEUE_SIZE);
+	if (ret != CELL_OK) {
+		fprintf(stderr, "sys_event_queue_create failed: %#.8x\n", ret);
+		exit(ret);
+	}
+	
+	/*
+	 * Create a PPU thread to handle SPU thread events.
+	 *
+	 * This thread receives and handles the SPU thread events.  
+	 * If an event is received from the terminating port (TERMINATE_PORT_KEY),
+	 * this thread exits.
+	 */
+	sys_ppu_thread_t my_thread_id;
+	int event_handle_prio;
+	const char *event_handle_name = "SPU Thread Event Handler";
+
+	sys_ppu_thread_get_id(&my_thread_id);
+	ret = sys_ppu_thread_get_priority(my_thread_id, &event_handle_prio);
+	if (ret != CELL_OK) {
+		fprintf(stderr, "sys_ppu_thread_get_priority failed: %#.8x\n", ret);
+		exit(ret);	
+	} else 	{
+		event_handle_prio += 1;
+	}
+
+	ret = sys_ppu_thread_create(&event_handle_thread, spu_thr_event_handler,
+								(uint64_t)queue, event_handle_prio,
+								4096, SYS_PPU_THREAD_CREATE_JOINABLE,
+								event_handle_name);
+	if (ret != CELL_OK) {
+		fprintf(stderr, "sys_ppu_thread_create failed: %#.8x\n", ret);
 		exit(ret);
 	}
 
@@ -70,6 +123,13 @@ int main(int argc, char** argv)
 		fprintf(stderr, "sys_spu_thread_group_create failed: %#.8x\n", ret);
 		exit(ret);
 	}
+
+	//Connect the Event to Event Queue
+	ret = sys_spu_thread_group_connect_event(group, queue, SYS_SPU_THREAD_GROUP_EVENT_EXCEPTION);
+	if (ret != CELL_OK) {
+		printf("sys_spu_thread_group_connect_event failed %x\n", ret);
+		exit(1);
+	}
 	
 	if (argc > 1)
 	{
@@ -79,8 +139,9 @@ int main(int argc, char** argv)
 		if (ret != CELL_OK) {
 			printf("sys_spu_image_open failed %x\n", ret);
 			exit(1);
+		}
 	}
-	}else
+	else
 	{
 		printf("ERROR:  no SPU image name found! \n");
 		printf("NOTICE: please pass the SPU image name as Command Line parameter \n");
@@ -119,6 +180,7 @@ int main(int argc, char** argv)
 
 	printf("All SPU threads have been successfully initialized.\n");
 	
+	
 	/*
 	 * Turn on the spu_printf
 	 */
@@ -143,7 +205,7 @@ int main(int argc, char** argv)
 		fprintf(stderr, "sys_spu_thread_group_start failed: %#.8x\n", ret);
 		exit(ret);
 	}
-	
+
 	/*
 	 * Wait for the termination of the SPU thread group
 	 */
@@ -185,6 +247,19 @@ int main(int argc, char** argv)
 
 	printf("The expected sum of all exit status is 300.\n");
 
+	//Disconnect the Event to Event Queue
+	ret = sys_spu_thread_group_disconnect_event(group, SYS_SPU_THREAD_GROUP_EVENT_EXCEPTION);
+	if (ret != CELL_OK) {
+		printf("sys_spu_thread_group_disconnect_event failed %x\n", ret);
+		exit(1);
+	}
+
+	ret = sys_event_queue_destroy(queue, 0);
+	if (ret != CELL_OK) {
+		fprintf(stderr, "sys_event_queue_destroy failed: %#.8x\n", ret);
+	} else {
+		printf("Destroyed the event queue.\n");
+	}
 	/*
 	 * Destroy the SPU thread group
 	 */
@@ -204,4 +279,43 @@ int main(int argc, char** argv)
 	printf("Exiting.\n");
 
 	return 0;
+}
+
+/*
+ * SPU Thread Event handler
+ *
+ * \param queue_id Event queue ID (Casted to uint64_t)
+ */
+void spu_thr_event_handler(uint64_t uint64_t_queue_id) 
+{
+	sys_event_queue_t queue_id = uint64_t_queue_id;
+	sys_spu_thread_group_t source_grp_id;
+	int ret;
+
+	while (1) {
+		sys_event_t event;
+		ret = sys_event_queue_receive(queue_id, &event, SYS_NO_TIMEOUT);
+		if (ret != CELL_OK) {
+			if (ret == (int)ECANCELED) {
+				break;
+			} else {
+				fprintf(stderr, "sys_event_queue_receive failed: %#.8x\n", ret);
+				sys_ppu_thread_exit(ret);
+			}
+		}
+
+		/*
+		 * Multiplex the event source 
+		 */
+
+		if (event.source == SYS_SPU_THREAD_GROUP_EVENT_EXCEPTION_KEY) {
+			//TODO: To be replaced with real handling code
+			source_grp_id = (sys_spu_thread_group_t)event.data1;
+			printf("SPU exception detected.\n");
+		} else {
+			printf("Unexpected Event.\n");
+		}
+	}
+	printf("SPU Thread Event Handler is exiting.\n");
+	sys_ppu_thread_exit(0);
 }
