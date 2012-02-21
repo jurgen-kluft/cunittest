b919c768bfce0e50203ee3b3cf38506d7745c3f3
Li Jian" <lijian_a889@virtuos.com.cn>
2012-02-21 14:58:40 +0800

    * Modify the manual * Change SPU exception handling code.

---
+++ source/main/cpp/ut_TestState.cpp
@@ -0,0 +1,46 @@
+#include "xunittest\private\ut_TestState.h"
+
+UnitTest::TestState* UnitTest::TestState::sInstance = 0;
+
+namespace UnitTest
+{
+	TestState::TestState(int testCount, int failureCount)
+		:mExceptionTestCount(testCount)
+		,mExceptionFailureCount(failureCount)
+	{
+		mExceptionOccurred = (mExceptionTestCount != 0);
+	}
+
+	TestState::~TestState() 
+	{
+	}
+
+	void TestState::setTestState(int testCount, int failureCount)
+	{
+		mExceptionTestCount = testCount;
+		mExceptionFailureCount = failureCount;
+		mExceptionOccurred = (mExceptionTestCount != 0);	
+	}
+
+	void TestState::sCreateInstance()
+	{
+		if (0 == sInstance)
+		{
+			sInstance = new TestState();
+		}
+	}
+
+	TestState* TestState::sGetInstance()
+	{
+		return sInstance;
+	}
+
+	void TestState::sDestroyInstance()
+	{
+		if (0 != sInstance)
+		{
+			delete sInstance;
+			sInstance = 0;
+		}
+	}
+}
---
+++ source/main/include/xunittest/private/ut_TestState.h
@@ -0,0 +1,32 @@
+#ifndef __XUNITTEST_TESTSTATE_H__
+#define __XUNITTEST_TESTSTATE_H__
+
+namespace UnitTest
+{
+	class TestState
+	{
+	public:
+		~TestState();
+
+		void setTestState(int testCount, int failureCount);
+
+		bool hasExceptionOccurred() { return mExceptionOccurred; }
+		bool needToSkipTest(int testCount) { return mExceptionOccurred && (testCount < mExceptionTestCount); }
+		bool isTheCrashedTest(int testCount) { return mExceptionOccurred && (testCount == mExceptionTestCount); }
+		int  getExceptionFailureCount() { return mExceptionFailureCount; }
+
+		static void sCreateInstance();
+		static TestState* sGetInstance();
+		static void sDestroyInstance();
+
+	private:
+		TestState(int testCount = 0, int failureCount = 0);
+
+		static TestState*	sInstance;
+
+		bool				mExceptionOccurred;
+		int					mExceptionTestCount;
+		int					mExceptionFailureCount;
+	};
+}
+#endif	///<__XUNITTEST_TESTSTATE_H__
