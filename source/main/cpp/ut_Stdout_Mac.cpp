ab5f1f1b65200f52c782e3386f7cf8795a88ea53
jurgen.kluft <jurgen.kluft@gmail.com>
2023-04-27 08:39:00 +0800

    * cunittest; compiling/linking for Mac

--- source/main/cpp/ut_Stdout_Mac.cpp
+++ source/main/cpp/ut_Stdout_Mac.cpp
@@ -5,49 +5,49 @@
 #include <time.h>
 #include <stdio.h>
 
-#define MESSAGE_S(str, size)	str
-#define STRINGFORMAT			sprintf				// Here you can divert to a printf/string-formatting implementation
+#define MESSAGE_S(str, size)	str, size
+#define STRINGFORMAT			snprintf				// Here you can divert to a printf/string-formatting implementation
 
 namespace UnitTest
 {
-	void Stdout::StringFormat(char* inMessage, int inMessageSizeInBytes, const char* inFormatStr, float inValue)
+	void Stdout::StringFormat(char* inMessage, int inMaxMessageLength, const char* inFormatStr, float inValue)
 	{
-		STRINGFORMAT(MESSAGE_S(inMessage, inMessageSizeInBytes), inFormatStr, inValue);
+		STRINGFORMAT(MESSAGE_S(inMessage, inMaxMessageLength), inFormatStr, inValue);
 	}
 
-	void Stdout::StringFormat(char* inMessage, int inMessageSizeInBytes, const char* inFormatStr, int inValue)
+	void Stdout::StringFormat(char* inMessage, int inMaxMessageLength, const char* inFormatStr, int inValue)
 	{
-		STRINGFORMAT(MESSAGE_S(inMessage, inMessageSizeInBytes), inFormatStr, inValue);
+		STRINGFORMAT(MESSAGE_S(inMessage, inMaxMessageLength), inFormatStr, inValue);
 	}
 
-	void Stdout::StringFormat(char* inMessage, int inMessageSizeInBytes, const char* inFormatStr, int inValue, int inValue2)
+	void Stdout::StringFormat(char* inMessage, int inMaxMessageLength, const char* inFormatStr, int inValue, int inValue2)
 	{
-		STRINGFORMAT(MESSAGE_S(inMessage, inMessageSizeInBytes), inFormatStr, inValue, inValue2);
+		STRINGFORMAT(MESSAGE_S(inMessage, inMaxMessageLength), inFormatStr, inValue, inValue2);
 	}
 
-	void Stdout::StringFormat(char* inMessage, int inMessageSizeInBytes, const char* inFormatStr, int inValue, const char* inName)
+	void Stdout::StringFormat(char* inMessage, int inMaxMessageLength, const char* inFormatStr, int inValue, const char* inName)
 	{
-		STRINGFORMAT(MESSAGE_S(inMessage, inMessageSizeInBytes), inFormatStr, inValue, inName);
+		STRINGFORMAT(MESSAGE_S(inMessage, inMaxMessageLength), inFormatStr, inValue, inName);
 	}
 
-	void Stdout::StringFormat(char* inMessage, int inMessageSizeInBytes, const char* inFormatStr, const char* inFile, int inLine, const char* inTestName, const char* inFailure)
+	void Stdout::StringFormat(char* inMessage, int inMaxMessageLength, const char* inFormatStr, const char* inFile, int inLine, const char* inTestName, const char* inFailure)
 	{
-		STRINGFORMAT(MESSAGE_S(inMessage, inMessageSizeInBytes), inFormatStr, inFile, inLine, inTestName, inFailure);
+		STRINGFORMAT(MESSAGE_S(inMessage, inMaxMessageLength), inFormatStr, inFile, inLine, inTestName, inFailure);
 	}
 
 	void			Stdout::StringFormat(char* outMessage, int inMaxMessageLength, const char* inFormatStr, const char* inStr1)
 	{
-		STRINGFORMAT(MESSAGE_S(outMessage, inMessageSizeInBytes), inFormatStr, inStr1);
+		STRINGFORMAT(MESSAGE_S(outMessage, inMaxMessageLength), inFormatStr, inStr1);
 	}
 
 	void			Stdout::StringFormat(char* outMessage, int inMaxMessageLength, const char* inFormatStr, const char* inStr1, const char* inStr2, int inValue)
 	{
-		STRINGFORMAT(MESSAGE_S(outMessage, inMessageSizeInBytes), inFormatStr, inStr1, inStr2, inValue);
+		STRINGFORMAT(MESSAGE_S(outMessage, inMaxMessageLength), inFormatStr, inStr1, inStr2, inValue);
 	}
 
 	void			Stdout::StringFormat(char* outMessage, int inMaxMessageLength, const char* inFormatStr, const char* inStr1, const char* inStr2, const char* inStr3)
 	{
-		STRINGFORMAT(MESSAGE_S(outMessage, inMessageSizeInBytes), inFormatStr, inStr1, inStr2, inStr3);
+		STRINGFORMAT(MESSAGE_S(outMessage, inMaxMessageLength), inFormatStr, inStr1, inStr2, inStr3);
 	}
 
 
