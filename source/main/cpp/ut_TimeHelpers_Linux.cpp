f48245a404d37cc835ad09ae0bc20ce8435c04a2
jurgen.kluft <jurgen.kluft@gmail.com>
2024-10-12 17:13:10 +0800

    * ccunittest
    * linux timehelpers, namespace std

--- source/main/cpp/ut_TimeHelpers_Linux.cpp
+++ source/main/cpp/ut_TimeHelpers_Linux.cpp
@@ -13,20 +13,20 @@ namespace UnitTest
 
 	void g_InitTimer()
 	{
-        s_base = chrono::steady_clock::now();
+        s_base = std::chrono::steady_clock::now();
 	}
 
 	time_t g_TimeStart()
     {
-        uint64_t start = (uint64_t)chrono::steady_clock::now() - s_base;
+        uint64_t start = (uint64_t)std::chrono::steady_clock::now() - s_base;
         return *(time_t*)(&start);
 	}
 
 	double g_GetElapsedTimeInMs(time_t stamp)
 	{
 		uint64_t const last = s_base + *(uint64_t*)(&stamp);
-		uint64_t const current = chrono::steady_clock::now();
-		chrono::duration_cast<chrono::milliseconds>(current - last).count()
+		uint64_t const current = std::chrono::steady_clock::now();
+		std::chrono::duration_cast<std::chrono::milliseconds>(current - last).count()
 		return ms;
 	}
 
