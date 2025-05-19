a09f644214df91f59043ab3211e704f99d58849e
jurgen.kluft <jurgen.kluft@gmail.com>
2025-05-19 19:37:45 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -8,11 +8,11 @@ import (
 func GetPackage() *denv.Package {
 
 	// 'cunittest' library
-	mainlib := denv.SetupDefaultCppLibProject("cunittest", "github.com\\jurgen-kluft\\cunittest")
+	mainlib := denv.SetupCppLibProject("cunittest", "github.com\\jurgen-kluft\\cunittest")
 
 	// 'cunittest' test project
 	maintest := denv.SetupDefaultCppTestProject("cunittest_test", "github.com\\jurgen-kluft\\cunittest")
-	maintest.Dependencies = append(maintest.Dependencies, mainlib)
+	maintest.AddDependency(mainlib)
 
 	mainpkg := denv.NewPackage("cunittest")
 	mainpkg.AddMainLib(mainlib)
